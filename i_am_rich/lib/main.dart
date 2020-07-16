ad1a57236d2fc4fd78d9acf594a365c6ab1ff81b
moraesjeremias <moraesjeremias@gmail.com>
2020-07-16 01:36:56 -0300

    creating home screen

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[800],
+        appBar: AppBar(
+          title: Text("I'm Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage(
+              'https://wallpaperhd.wiki/wp-content/uploads/macbook-pro-retina-wallpaper-2880x1800-macbook-pro-retina-wallpaper-2880x1800-macbook-pro-retina-wallpaper-2880x1800-firewatch-4k-17-2880x1800.jpg'),
+        ),
+      ),
+    ),
+  );
 }
